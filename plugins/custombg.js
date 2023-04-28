72f22b5a31347d9c826a826e4ebd9fc19bfdc9fc
AlirezaKJ <akarimijafari@gmail.com>
2023-04-28 21:56:14 +1000

    custombg script opacity and bg override fix

--- plugins/custombg.js
+++ plugins/custombg.js
@@ -1,9 +1,8 @@
-// Dont do anything if you dont know anything ;)
-
 let appcontainer = document.querySelector("#app")
 let contentcontainer = document.querySelector("#app .l-content")
 contentcontainer.style.zindex = "2"
 contentcontainer.style.position = "relative"
+contentcontainer.style.backgroundColor = "#ffffff"
 let custombg = document.createElement("div")
 custombg.classList.add("custombg")
 custombg.style.width = "100%"
@@ -15,6 +14,7 @@ custombg.style.zindex = "1"
 custombg.style.backgroundRepeat = "no-repeat"
 custombg.style.backgroundPosition = "center"
 custombg.style.backgroundSize = "cover"
+custombg.style.filter = "opacity(0.8)"
 custombg.innerHTML = "<div class='bgblurrer'></div>"
 appcontainer.prepend(custombg)
 let custombgblurrer = document.querySelector("#app .custombg .bgblurrer")
