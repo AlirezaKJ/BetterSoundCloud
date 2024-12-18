5e5fe846a8dfb9488f0b402cffde706b11e3518f
AlirezaKJ <akarimijafari@gmail.com>
2024-12-18 17:15:52 +1100

    settings base functions

--- app/lib/js/settings.js
+++ app/lib/js/settings.js
@@ -1,8 +1,26 @@
 let settings = {
-  activetheme: "default",
-  adblock: true,
-  loadingscreen: true,
-  custommediakeys: true,
-  zoomfactor: 1,
-  discordrpc: true,
+  custombg: dbResolve("settings.custombg", true),
+  discordrpc:dbResolve("settings.discordrpc", true),
+  bindctrlr: dbResolve("settings.bindctrlr", "reloadview"),
+  bindf5: dbResolve("settings.bindf5", undefined),
+  startupfullscreen: dbResolve("settings.startupfullscreen", false),
+  startuplastpage: dbResolve("settings.startuplastpage", false),
+  startupurl: dbResolve("settings.startupurl", false),
+  scrollerbtn: dbResolve("settings.scrollerbtn", false),
+}
+
+function updateLS() {
+  dbSetItem("settings.custombg", settings.custombg)
+  dbSetItem("settings.discordrpc", settings.discordrpc)
+  dbSetItem("settings.bindctrlr", settings.bindctrlr)
+  dbSetItem("settings.bindf5", settings.bindf5)
+  dbSetItem("settings.startupfullscreen", settings.startupfullscreen)
+  dbSetItem("settings.startuplastpage", settings.startuplastpage)
+  dbSetItem("settings.startupurl", settings.startupurl)
+  dbSetItem("settings.scrollerbtn", settings.scrollerbtn)
+}
+
+function changeSettings(name, value) {
+  settings[name] = value
+  updateLS()
 }
