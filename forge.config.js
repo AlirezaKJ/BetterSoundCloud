5e701c5be3a4cc6ed149afd133037fa30f1b72a5
Mustafa <120393372+droptablemustafa@users.noreply.github.com>
2025-11-22 16:09:46 +0000

    Add macOS icon
    Added icon.icns for macOS to app assets and updated the Electron Forge packager config to use the new icon path, improving cross-platform icon support.

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: "app/lib/assets/icon.ico",
+    icon: "app/lib/assets/icon",
   },
   // files: [
   //   'dist/**/*',
